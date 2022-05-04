981f18ec8b558049c7668a5ceb4082a0838cf0eb
parkhayoung615 <parkhayoung615@gmail.com>
2022-05-04 10:12:47 +0900

    Update system.sql
    2022.05.04 2 commit

--- 220504/H20104/WebContent/system.sql
+++ 220504/H20104/WebContent/system.sql
@@ -1,4 +1,8 @@
 create table test(id varchar2(5), pwd varchar(5));
 
+insert into test values('aa', '11');
+insert into test values('bb', '22');
+insert into test values('cc', '33');
+
 
 select * FROM test;
