8f4faad6354946cbf9e6a1ae5c287b66b1ba5620
parkhayoung615 <parkhayoung615@gmail.com>
2022-05-18 10:58:54 +0900

    Update Project
    2022.05.18 7 commit

--- 220518/H20104/WebContent/query.sql
+++ 220518/H20104/WebContent/query.sql
@@ -1,1 +1,11 @@
 CREATE TABLE test(id varchar2(5), pwd varchar(5));
+
+insert into test values('aa', '11');
+insert into test values('bb', '22');
+insert into test values('cc', '33');
+insert into test values('dd', '44');
+
+update test SET pwd = 999 WHERE id='dd';
+update test set id='zz' WHERE pwd = 100;
+
+SELECT * FROM test;
